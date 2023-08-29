d2003684f2514f7e50273c572a908438b5a5c639
bernsoa <soane.bernon@divtec.ch>
2023-08-29 15:40:51 +0200

    ajout jshintrc

--- js/main.js
+++ js/main.js
@@ -2,8 +2,12 @@
  * Mon premier JS
  * @author Soane Bernon
  * @version 0.0.1
- * @since 2023-08-09
+ * @since 2023-08-29
  */
 
+"use strict";
+
 let msg = "Bonjour";
+if (msg == "pierre" || msg == "Bonjour")
+    msg = "SALAMALEKOUM";
 console.log(msg);
